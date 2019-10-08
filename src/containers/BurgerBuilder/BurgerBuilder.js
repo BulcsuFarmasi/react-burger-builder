1837358647eb2a9b7796fa719973d1cd857425c8
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-10-08 21:35:30 +0200

    remove ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,7 +23,6 @@ class BurgerBuilder extends Component {
   };
 
   addIngredientHandler = type => {
-    console.log(this.state);
     const oldCount = this.state.ingredients[type];
     const updatedCount = oldCount + 1;
     const updatedIngredients = {
@@ -39,13 +38,40 @@ class BurgerBuilder extends Component {
     });
   };
 
-  removeIngredientHandler(type) {}
+  removeIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
+    }
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: newPrice
+    });
+  };
 
   render() {
+    const disableInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disableInfo) {
+      disableInfo[key] = disableInfo[key] <= 0;
+    }
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls ingredientAdded={this.addIngredientHandler} />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disableInfo}
+        />
       </Aux>
     );
   }
