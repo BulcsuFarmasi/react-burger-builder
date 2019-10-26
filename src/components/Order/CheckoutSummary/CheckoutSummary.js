fae81f283e47b8c6c71ec47ccf377a807575ed50
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-10-26 15:23:52 +0200

    use checkout

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -5,11 +5,11 @@ import Button from "../../UI/Button/Button";
 
 import classes from "./CheckoutSummary.module.css";
 
-const checkoutSummary = () => {
+const checkoutSummary = props => {
   return (
-    <div className={classes.checkoutSummary}>
+    <div className={classes.CheckoutSummary}>
       <h1>We hope it tastes well!</h1>
-      <div style={{ width: "300px", height: "300px", margin: "auto" }}>
+      <div style={{ width: "100%", margin: "auto" }}>
         <Burger ingredients={props.ingredients} />
       </div>
       <Button btnType="Danger" clicked>
