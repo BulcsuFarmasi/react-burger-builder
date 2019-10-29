fc841a7d55041bf4aca492adb87b3d029c7322e5
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-10-29 21:21:18 +0100

    create and use checkout data

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 
+import { Route } from "react-router-dom";
+
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import ContactData from "./ContactData/ContactData";
 
 class Checkout extends Component {
   state = {
@@ -26,7 +29,7 @@ class Checkout extends Component {
   };
 
   checkoutContinuedHandler = () => {
-    this.props.history.replace("/checkout-contact-data");
+    this.props.history.replace("/checkout/contact-data");
   };
 
   render() {
@@ -36,6 +39,10 @@ class Checkout extends Component {
           ingredients={this.state.ingredients}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
+        />
+        <Route
+          path={this.props.match.path + "/contact-data"}
+          component={ContactData}
         />
       </div>
     );
