29e12ee7d90aa0f9a31ddc82031a216d2c84dee7
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-12-02 21:58:26 +0100

    redirecting users to checkout page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -38,6 +38,12 @@ class Auth extends Component {
     isSignup: true
   };
 
+  componentDidMount() {
+    if (!this.props.buildingBurger && this.props.authRedirectPath !== "/") {
+      this.props.onSetAuthRedirect();
+    }
+  }
+
   checkValidity(value, rules) {
     let isValid = true;
     if (!rules) {
@@ -134,7 +140,8 @@ class Auth extends Component {
     let authRedirect = null;
 
     if (this.props.isAuthenticated) {
-      authRedirect = <Redirect to="/" />;
+      console.log(this.props.authRedirectPath);
+      authRedirect = <Redirect to={this.props.authRedirectPath} />;
     }
 
     return (
@@ -157,14 +164,17 @@ const mapStateToProps = state => {
   return {
     loading: state.auth.loading,
     error: state.auth.error,
-    isAuthenticated: state.auth.token != null
+    isAuthenticated: state.auth.token != null,
+    buildingBurger: state.burgerBuilder.building,
+    authRedirectPath: state.auth.authRedirect
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     onAuth: (email, password, isSignup) =>
-      dispatch(actions.auth(email, password, isSignup))
+      dispatch(actions.auth(email, password, isSignup)),
+    onSetAuthRedirect: () => dispatch(actions.setAuthRedirectPath("/"))
   };
 };
 
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -6,4 +6,4 @@ export {
 
 export { fetchOrders, purchaseBurger, purchaseInit } from "./order";
 
-export { auth, logout } from "./auth";
+export { auth, logout, setAuthRedirectPath } from "./auth";
