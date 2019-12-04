8a13d8429dc641ed1657c823412bbd987bfd0ce2
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-12-04 23:20:52 +0100

    remove console log statements

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,7 +6,6 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngedient";
 
 class Burger extends Component {
   render() {
-    console.log(this.props);
     let transformedIngredients = Object.keys(this.props.ingredients)
       .map(igKey => {
         return [...Array(this.props.ingredients[igKey])].map((_, i) => {
